e23f6da802edf32aa730d404c9d1d3c2353c4fe3
allenji <weifengji@yahoo.com>
2015-12-12 14:46:18 -0500

    redefine CandidateJet to avoid dependency on reco::Candidate

--- Collection/interface/CandidateJet.h
+++ Collection/interface/CandidateJet.h
@@ -1,12 +1,29 @@
 #ifndef CANDIDATEJET_H 
 #define CANDIDATEJET_H 
-#include "DataFormats/JetReco/interface/CaloJet.h" 
-class CandidateJet : public reco::CaloJet {
+class CandidateJet {
   public:
-    CandidateJet();
-    CandidateJet(const reco::CaloJet &);
-    ~CandidateJet();
+    void set_energy(double energy) {energy_ = energy;}
+    void set_et(double et) {et_ = et;}
+    void set_eta(double eta) {eta_ = eta;}
+    void set_phi(double phi) {phi_ = phi;}
+    void set_n60(double n60) {n60_ = n60;}
+    void set_n90(double n90) {n90_ = n90;}
+
+    const double energy() const {return energy_;}
+    const double et() const {return et_;}
+    const double eta() const {return eta_;}
+    const double phi() const {return phi_;}
+    const double n60() const {return n60_;}
+    const double n90() const {return n90_;}
+  private:
+    double energy_;
+    double et_;
+    double eta_;
+    double phi_;
+    double n60_;
+    double n90_;
 };
+
 
 #endif
 
