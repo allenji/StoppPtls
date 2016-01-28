5e199471403bb476790dec641d22ccb9c3bf345e
allenji <weifengji@yahoo.com>
2016-01-28 15:59:54 -0500

    accomodated for the AOD_CUSTOM data format of the framework, some dirty tricks included

--- StandardAnalysis/interface/CustomDataFormat.h
+++ StandardAnalysis/interface/CustomDataFormat.h
@@ -4,7 +4,7 @@
 
 #ifndef CUSTOM_DATA_FORMAT
 
-  #include "OSUT3Analysis/AnaTools/interface/DataFormatMiniAOD.h"
+  #include "OSUT3Analysis/AnaTools/interface/DataFormatAOD.h"
 
   #undef jets_TYPE
 
@@ -13,12 +13,49 @@
   #define events_TYPE CandidateEvent
 
   #undef events_INVALID
+
+  #undef cschits_TYPE
+  #undef cscsegs_TYPE
+  #undef dtsegs_TYPE
+  #undef rpchits_TYPE
   
   #define cschits_TYPE CandidateCscHit
   #define cscsegs_TYPE CandidateCscSeg
   #define dtsegs_TYPE CandidateDTSeg
   #define rpchits_TYPE CandidateRpcHit
 
+  #undef cschits_INVALID
+  #undef cscsegs_INVALID
+  #undef dtsegs_INVALID
+  #undef rpchits_INVALID
+// Redefining to avoid editing the objectProducer right now, need to do it later...
+  #undef electrons_TYPE
+  #define electrons_TYPE INVALID_TYPE
+  #define electrons_INVALID
+
+  #undef basicjets_TYPE
+  #define basicjets_TYPE INVALID_TYPE
+  #define basicjets_INVALID
+
+  #undef muons_TYPE
+  #define muons_TYPE INVALID_TYPE
+  #define muons_INVALID
+
+  #undef photons_TYPE
+  #define photons_TYPE INVALID_TYPE
+  #define photons_INVALID
+
+  #undef taus_TYPE
+  #define taus_TYPE INVALID_TYPE
+  #define taus_INVALID
+
+  #undef tracks_TYPE
+  #define tracks_TYPE INVALID_TYPE
+  #define tracks_INVALID
+
+  #undef prescales_TYPE
+  #define prescales_TYPE pat::PackedTriggerPrescales
+  #undef prescales_INVALID
   #include "DataFormats/JetReco/interface/CaloJet.h"
   #include "StoppPtls/Collection/interface/CandidateCscHit.h"
   #include "StoppPtls/Collection/interface/CandidateCscSeg.h"
@@ -26,4 +63,24 @@
   #include "StoppPtls/Collection/interface/CandidateEvent.h"
   #include "StoppPtls/Collection/interface/CandidateJet.h"
   #include "StoppPtls/Collection/interface/CandidateRpcHit.h"
+// a dirty trick to avoid bug in the InfoPrinter.cc, which required a prescale, which I cannot find temporarily for AOD format.
+  #include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
+// a dirty trick to avoid complie error in the  OriginalFormatProducer.cc, included all header we need for MINI_AOD
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/EgammaReco/interface/SuperCluster.h"
+#include "DataFormats/JetReco/interface/BasicJet.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
+#include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
+#include "DataFormats/PatCandidates/interface/Photon.h"
+#include "DataFormats/PatCandidates/interface/Tau.h"
+#include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h"
 #endif
