aaefbdac3df18676f6e561714f60440b30b8596e
allenji <weifengji@yahoo.com>
2016-02-02 00:31:03 -0500

    add dataset register source file, second jet analysis batch cfg, local config, update myComparisonPlotConfig.py

---
+++ StandardAnalysis/python/localConfig.py
@@ -0,0 +1,25 @@
+dataset_names = {
+    'GluinoToUUbarChi0_1200_200'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_200/USER",
+    'GluinoToUUbarChi0_1200_600'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_600/USER",
+    'GluinoToUUbarChi0_1200_1000'  : "/HSCPgluino_UUbarChi0_13TeV-pythia6/RunIISpring15-74x_mcRun2_StoppPtls_gluino_1200_chi0_1000/USER",
+
+}
+
+nJobs = {
+    'GluinoToUUbarChi0_1200_200' : 1,
+    'GluinoToUUbarChi0_1200_600' : 1,
+    'GluinoToUUbarChi0_1200_1000': 1,
+}
+
+
+maxEvents = {
+    'GluinoToUUbarChi0_1200_200' : -1,
+    'GluinoToUUbarChi0_1200_600' : -1,
+    'GluinoToUUbarChi0_1200_1000': -1,
+}
+
+types = {
+    'GluinoToUUbarChi0_1200_200' : "signalMC",
+    'GluinoToUUbarChi0_1200_600' : "signalMC",
+    'GluinoToUUbarChi0_1200_1000' : "signalMC",
+}
---
+++ StandardAnalysis/test/SecondJetAnalysis_batch.py
@@ -0,0 +1,11 @@
+from StoppPtls.StandardAnalysis.localConfig import *
+
+config_file = "SecondJetAnalysis.py"
+
+datasets = [
+  'GluinoToUUbarChi0_1200_200',
+  'GluinoToUUbarChi0_1200_600',
+  'GluinoToUUbarChi0_1200_1000'
+]
+
+InputConforArguments = {}
