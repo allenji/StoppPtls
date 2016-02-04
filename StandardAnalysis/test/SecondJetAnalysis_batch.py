d198c11225a78e8deb46d34ce84123ed8494d846
allenji <weifengji@yahoo.com>
2016-02-03 23:13:48 -0500

    modify files included

--- StandardAnalysis/test/SecondJetAnalysis_batch.py
+++ StandardAnalysis/test/SecondJetAnalysis_batch.py
@@ -1,11 +1,17 @@
-from StoppPtls.StandardAnalysis.localConfig import *
+from OSUT3Analysis.Configuration.configurationOptions import *
+from OSUT3Analysis.Configuration.RECOSamples import *
 
 config_file = "SecondJetAnalysis.py"
 
 datasets = [
   'GluinoToUUbarChi0_1200_200',
   'GluinoToUUbarChi0_1200_600',
-  'GluinoToUUbarChi0_1200_1000'
+  'GluinoToUUbarChi0_1200_1000',
+  'GluinoToGChi0_1200_200',
+  'GluinoToGChi0_1200_400',
+  'GluinoToGChi0_1200_600',
+  'GluinoToGChi0_1200_800',
+  'GluinoToGChi0_1200_1000',
 ]
 
-InputConforArguments = {}
+InputCondorArguments = {}
