52c633013039bc72792b21b4c34bf2b8591e6d84
allenji <weifengji@yahoo.com>
2016-03-31 11:08:50 -0400

    add reco eff of stop

--- Analysis/scripts/energyscan_fit.py
+++ Analysis/scripts/energyscan_fit.py
@@ -2,6 +2,7 @@ import ROOT as rt
 import array
 from math import *
 #Run2 signal simulation
+#Gluino counting
 x_G600 = [50.,75.,85.,95.,105.,150.,195.,220]
 count_G600 = [18, 703, 1180, 1540, 1940, 2410, 2550, 2550]
 total_G600 = [5390.,5390.,5390.,5390.,5390.,5390.,5390.,5390.]
@@ -58,9 +59,63 @@ graph_GluinoPlus = rt.TGraphErrors(len(allx_Gluino), convert_to_array(allx_Gluin
                                    convert_to_array(len(allx_Gluino)*[0]), convert_to_array(len(allx_Gluino)*[0]))
 graph_GluinoMinus = rt.TGraphErrors(len(allx_Gluino), convert_to_array(allx_Gluino), convert_to_array(ally_Gminus1),
                                     convert_to_array(len(allx_Gluino)*[0]), convert_to_array(len(allx_Gluino)*[0]))
-#graph_GluinoPlus = rt.TGraphErrors(len(allx_Gluino), allx_Gluino, ally_Gplus1, 0, 0)
-#graph_GluinoMinus = rt.TGraphErrors(len(allx_Gluino), allx_Gluino, ally_Gminus1, 0, 0)
+#Stop counting
+x_S400 = [98., 111., 141., 189., 210., 231.]
+count_S400 = [881, 1120, 1380, 1620, 1650, 1690]
+total_S400 = [4800., 4800., 4800., 4800., 4800., 4800.]
 
+x_S600 = [99., 192., 251., 299.]
+count_S600 = [837, 1490, 1610, 1580]
+total_S600 = [4450., 4450., 4450., 4450.]
+
+x_S1000 = [250., 300., 351., 400., 450., 500., 510.]
+count_S1000 = [1520, 1530, 1490, 1110, 1040, 1430, 697]
+total_S1000 = [4210., 4210., 4210., 3130., 3130., 4210., 2030.]
+
+allx_Stop = x_S400 + x_S600 + x_S1000
+allcount_Stop = count_S400 + count_S600 + count_S1000
+alltotal_Stop = total_S400 + total_S600 + total_S1000
+allzip_Stop = zip(allx_Stop, allcount_Stop, alltotal_Stop)
+allzip_Stop.sort()
+allx_Stop = [x for x, y, z in allzip_Stop]
+allcount_Stop = [y for x, y, z in allzip_Stop]
+alltotal_Stop = [z for x, y, z in allzip_Stop]
+
+ally_Stop, allerr_Stop, ally_Splus1, ally_Sminus1 = [], [], [], []
+for i in range(len(allx_Stop)):
+    ally_Stop.append(allcount_Stop[i]/alltotal_Stop[i])
+    allerr_Stop.append(ally_Stop[i] * rt.TMath.Sqrt(1./allcount_Stop[i] + 1./alltotal_Stop[i]))
+    ally_Splus1.append(ally_Stop[i] + allerr_Stop[i])
+    ally_Sminus1.append(ally_Stop[i] - allerr_Stop[i])
+
+y_S400, err_S400, y_S600, err_S600, y_S1000, err_S1000 = [], [], [], [], [], []
+for i in range(len(x_S400)):
+    y_S400.append(count_S400[i]/total_S400[i])
+    err_S400.append(y_S400[i] * rt.TMath.Sqrt(1./count_S400[i] + 1./total_S400[i]))
+
+for i in range(len(x_S600)):
+    y_S600.append(count_S600[i]/total_S600[i])
+    err_S600.append(y_S600[i] * rt.TMath.Sqrt(1./count_S600[i] + 1./total_S600[i]))
+
+for i in range(len(x_S1000)):
+    y_S1000.append(count_S1000[i]/total_S1000[i])
+    err_S1000.append(y_S1000[i] * rt.TMath.Sqrt(1./count_S1000[i] + 1./total_S1000[i]))
+
+graph_S400 = rt.TGraphErrors(len(x_S400), convert_to_array(x_S400), convert_to_array(y_S400),
+                             convert_to_array(len(x_S400)*[0]), convert_to_array(err_S400))
+graph_S600 = rt.TGraphErrors(len(x_S600), convert_to_array(x_S600), convert_to_array(y_S600),
+                              convert_to_array(len(x_S600)*[0]), convert_to_array(err_S600))
+graph_S1000 = rt.TGraphErrors(len(x_S1000), convert_to_array(x_S1000), convert_to_array(y_S1000),
+                              convert_to_array(len(x_S1000)*[0]), convert_to_array(err_S1000))
+graph_Stop = rt.TGraphErrors(len(allx_Stop), convert_to_array(allx_Stop), convert_to_array(ally_Stop),
+                               convert_to_array(len(allx_Stop)*[0]), convert_to_array(len(allx_Stop)*[0]))
+graph_StopPlus = rt.TGraphErrors(len(allx_Stop), convert_to_array(allx_Stop), convert_to_array(ally_Splus1),
+                                   convert_to_array(len(allx_Stop)*[0]), convert_to_array(len(allx_Stop)*[0]))
+graph_StopMinus = rt.TGraphErrors(len(allx_Stop), convert_to_array(allx_Stop), convert_to_array(ally_Sminus1),
+                                    convert_to_array(len(allx_Stop)*[0]), convert_to_array(len(allx_Stop)*[0]))
+
+#making plot
+#Gluino
 canvas = rt.TCanvas("reco_eff", "reco_eff", 50, 50, 800, 600)
 outputfile = rt.TFile("energyscan_fit_13TeV.root","RECREATE")
 canvas.cd()
@@ -118,6 +173,55 @@ graph_Gluino.Fit(fit_Gluino)
 graph_GluinoPlus.Fit(fit_GluinoPlus)
 graph_GluinoMinus.Fit(fit_GluinoMinus)
 
+#Stop
+graph_S400.SetLineColor(rt.kBlue + 1)
+graph_S400.SetMarkerColor(rt.kBlue + 1)
+graph_S400.SetMarkerStyle(22)
+graph_S400.SetMarkerSize(1.15)
+graph_S400.SetLineWidth(2)
+
+graph_S600.SetLineColor(rt.kBlue + 3)
+graph_S600.SetMarkerColor(rt.kBlue + 3)
+graph_S600.SetMarkerStyle(21)
+graph_S600.SetMarkerSize(1.15)
+graph_S600.SetLineWidth(2)
+
+graph_S1000.SetLineColor(rt.kCyan + 3)
+graph_S1000.SetMarkerColor(rt.kCyan + 3)
+graph_S1000.SetMarkerStyle(34)
+graph_S1000.SetMarkerSize(1.15)
+graph_S1000.SetLineWidth(2)
+
+fit_Stop = rt.TF1("sfit", "[0]*TMath::Erf([1]*x-[2])+[3]", 0, 400)
+fit_Stop.SetParameters(20, 0.001, -1, -22)
+fit_Stop.SetLineColor(rt.kBlue - 9)
+fit_Stop.SetRange(50, 400)
+fit_StopPlus = rt.TF1("sfitp", "[0]*TMath::Erf([1]*x-[2])+[3]", 0, 400)
+fit_StopPlus.SetParameters(20, 0.001, -1, -22)
+fit_StopPlus.SetRange(50, 400)
+fit_StopPlus.SetFillStyle(1001)
+fit_StopPlus.SetFillColor(rt.kBlue -10)
+fit_StopPlus.SetLineColor(rt.kBlue -10)
+fit_StopPlus.SetLineWidth(7)
+fit_StopPlus.Draw("FC same")
+
+fit_StopMinus = rt.TF1("sfitm", "[0]*TMath::Erf([1]*x-[2])+[3]", 0, 400)
+fit_StopMinus.SetParameters(20, 0.001, -1, -22)
+fit_StopMinus.SetFillStyle(1001)
+fit_StopMinus.SetFillColor(10)
+fit_StopMinus.SetLineColor(rt.kBlue - 10)
+fit_StopMinus.SetRange(50, 400)
+fit_StopMinus.SetLineWidth(7)
+fit_StopMinus.Draw("FC same")
+
+graph_S400.Draw("P same")
+graph_S600.Draw("P same")
+graph_S1000.Draw("P same")
+
+graph_Stop.Fit(fit_Stop)
+graph_StopPlus.Fit(fit_StopPlus)
+graph_StopMinus.Fit(fit_StopMinus)
+
 gluino_max = fit_Gluino.GetMaximum()
 print "============== gluino max : " + str(gluino_max) + "=================="
 gluino_avg = 0
@@ -129,6 +233,18 @@ for i in range (10, len(allx_Gluino)):
         gluino_syst = fabs(ally_Gluino[i]-gluino_max)
 gluino_avg = gluino_avg/(len(allx_Gluino)-10)
 print "Systematic error on reco eff: " + str(gluino_syst/gluino_max)
+print "\n"
+stop_max = fit_Stop.GetMaximum()
+print "============== stop max : " + str(stop_max) + "=================="
+stop_avg = 0
+stop_syst = 0
+for i in range (4, len(allx_Stop)):
+    print str(ally_Stop[i]) + " - " + str(stop_max) + " = " + str(fabs(ally_Stop[i]-stop_max))
+    stop_avg = stop_avg + fabs(ally_Stop[i]-stop_max)
+    if fabs(ally_Stop[i]-stop_max) > stop_syst:
+        stop_syst = fabs(ally_Stop[i]-stop_max)
+stop_avg = stop_avg/(len(allx_Stop)-4)
+print "Systematic error on reco eff: " + str(stop_syst/stop_max)
 
 
 legend = rt.TLegend(0.55,0.15,0.9,0.45,"","brNDC")
@@ -137,6 +253,9 @@ legend.SetHeader("CMS Simulation,  #sqrt{s} = 13 TeV")
 legend.AddEntry(graph_G600, "m_{#tilde{g}} = 600 GeV","ep")
 legend.AddEntry(graph_G1200, "m_{#tilde{g}} = 1200 GeV","ep")
 legend.AddEntry(graph_G1800, "m_{#tilde{g}} = 1800 GeV","ep")
+legend.AddEntry(graph_S400, "m_{#tilde{t}} = 400 GeV","ep")
+legend.AddEntry(graph_S600, "m_{#tilde{t}} = 600 GeV","ep")
+legend.AddEntry(graph_S1000, "m_{#tilde{t}} = 1000 GeV","ep")
 legend.SetFillColor(rt.kWhite)
 legend.SetBorderSize(0)
 legend.Draw()
@@ -146,15 +265,3 @@ canvas.RedrawAxis()
 canvas.Write()
 canvas.Print("energyscan_fit_13TeV.pdf")
 outputfile.Close()
-
-
-def test():
-    print allx_Gluino
-#    print allcount_Gluino
-#    print alltotal_Gluino
-#    print ally_Gluino
-#    print allerr_Gluino
-#    print ally_Gplus1
-#    print ally_Gminus1
- 
-test()
