298b31b6d084aafa68b4e9e7102d5e6e39c1ca6b
allenji <weifengji@yahoo.com>
2016-04-18 22:37:53 -0400

    first step of migrating toyMC and limit plot making codes to new framework, old recipe works but new interface not yet implemented

---
+++ Statistics/src/classes.h
@@ -0,0 +1,10 @@
+#ifndef Statistics_classes_h
+#define Statistics_classes_h
+
+//#include "StoppedHSCP/Statistics/interface/BayesianCountingExperiment.h"
+//#include "StoppPtls/Statistics/interface/CL95CMSCountingExperiment.h"
+#include "StoppPtls/Statistics/interface/CLsCountingExperiment.h"
+//#include "StoppedHSCP/Statistics/interface/CLsbCountingExperiment.h"
+//#include "StoppedHSCP/Statistics/interface/FRCountingExperiment.h"
+
+#endif
