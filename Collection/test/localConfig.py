a9a80bea24ad0359b5e4f344eff75aee861556fc
Juliette Alimena <juliette.alimena@cern.ch>
2016-05-23 09:53:01 -0400

    register data and mchamp datasets

---
+++ Collection/test/localConfig.py
@@ -0,0 +1,21 @@
+from OSUT3Analysis.Configuration.configurationOptions import *
+from OSUT3Analysis.Configuration.RECOSamples import *
+
+intLumi = 2460
+
+config_file = "delayedMuonsCandiate_MC_cfg.py"
+
+datasets = [
+    #'mchamp100_RecoSeparateEventsParticle0',
+    #'mchamp200_RecoSeparateEventsParticle0',
+    #'mchamp400_RecoSeparateEventsParticle0',
+    'mchamp600_RecoSeparateEventsParticle0',
+    #'mchamp800_RecoSeparateEventsParticle0',
+    #'mchamp1000_RecoSeparateEventsParticle0',
+    #'mchamp1400_RecoSeparateEventsParticle0',
+    #'mchamp1800_RecoSeparateEventsParticle0',
+    #'mchamp2200_RecoSeparateEventsParticle0',
+    #'mchamp2600_RecoSeparateEventsParticle0',
+]
+    
+InputCondorArguments = {}
---
+++ Collection/test/localConfig_data.py
@@ -0,0 +1,12 @@
+from OSUT3Analysis.Configuration.configurationOptions import *
+from OSUT3Analysis.Configuration.RECOSamples import *
+
+intLumi = 2830
+
+config_file = "delayedMuonsCandiate_cfg.py"
+
+datasets = [
+    "NoBPTX_16Dec2015"
+]
+    
+InputCondorArguments = {}
