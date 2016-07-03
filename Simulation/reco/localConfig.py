91c5f85a9f71c46d43308003d17db4447dfdbaac
Juliette Alimena <juliette.alimena@cern.ch>
2016-07-03 14:06:29 -0400

    run over particle1

--- Simulation/reco/localConfig.py
+++ Simulation/reco/localConfig.py
@@ -17,6 +17,17 @@ datasets = [
     #'mchamp2200_DigiHltSeparateEventsParticle0',
     #'mchamp2600_DigiHltSeparateEventsParticle0',
 
+    #'mchamp100_DigiHltSeparateEventsParticle1',
+    #'mchamp200_DigiHltSeparateEventsParticle1',
+    #'mchamp400_DigiHltSeparateEventsParticle1',
+    #'mchamp600_DigiHltSeparateEventsParticle1',
+    #'mchamp800_DigiHltSeparateEventsParticle1',
+    #'mchamp1000_DigiHltSeparateEventsParticle1',
+    #'mchamp1400_DigiHltSeparateEventsParticle1',
+    #'mchamp1800_DigiHltSeparateEventsParticle1',
+    #'mchamp2200_DigiHltSeparateEventsParticle1',
+    #'mchamp2600_DigiHltSeparateEventsParticle1',
+
     #'gluino100_DigiHltSeparateEventsParticle0',
     #'gluino200_DigiHltSeparateEventsParticle0',
     #'gluino400_DigiHltSeparateEventsParticle0',
@@ -27,10 +38,25 @@ datasets = [
     #'gluino1400_DigiHltSeparateEventsParticle0'
     #'gluino1600_DigiHltSeparateEventsParticle0'
     #'gluino1800_DigiHltSeparateEventsParticle0'
-    'gluino2000_DigiHltSeparateEventsParticle0',
+    #'gluino2000_DigiHltSeparateEventsParticle0',
     #'gluino2200_DigiHltSeparateEventsParticle0',
     #'gluino2400_DigiHltSeparateEventsParticle0',
     #'gluino2600_DigiHltSeparateEventsParticle0',
+
+    #'gluino100_DigiHltSeparateEventsParticle1',
+    #'gluino200_DigiHltSeparateEventsParticle1',
+    #'gluino400_DigiHltSeparateEventsParticle1',
+    #'gluino600_DigiHltSeparateEventsParticle1',
+    #'gluino800_DigiHltSeparateEventsParticle1',
+    #'gluino1000_DigiHltSeparateEventsParticle1'
+    #'gluino1200_DigiHltSeparateEventsParticle1'
+    #'gluino1400_DigiHltSeparateEventsParticle1'
+    #'gluino1600_DigiHltSeparateEventsParticle1'
+    #'gluino1800_DigiHltSeparateEventsParticle1'
+    'gluino2000_DigiHltSeparateEventsParticle1',
+    #'gluino2200_DigiHltSeparateEventsParticle1',
+    #'gluino2400_DigiHltSeparateEventsParticle1',
+    #'gluino2600_DigiHltSeparateEventsParticle1',
 ]
     
 InputCondorArguments = {}
