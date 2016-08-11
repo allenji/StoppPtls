2c7a4f1b0b9b8921daae184ec9826ba2fe175b2c
Juliette Alimena <juliette.alimena@cern.ch>
2016-08-11 08:41:46 -0400

    make histos from stage1 directly

---
+++ DelayedMuonsAnalysis/test/localConfig_Stage1.py
@@ -0,0 +1,36 @@
+from OSUT3Analysis.Configuration.configurationOptions import *
+from OSUT3Analysis.Configuration.GENSIMSamples import *
+
+intLumi = 2460
+
+config_file = "Stage1Plots.py"
+
+datasets = [
+    #'mchamp100',
+    #'mchamp200',
+    #'mchamp400',
+    #'mchamp600',
+    #'mchamp800',
+    #'mchamp1000',
+    #'mchamp1400',
+    #'mchamp1800',
+    #'mchamp2200',
+    #'mchamp2600',
+
+    #'gluino100',
+    'gluino200',
+    #'gluino400',
+    #'gluino600',
+    #'gluino800',
+    'gluino1000',
+    #'gluino1200',
+    #'gluino1400',
+    #'gluino1600',
+    #'gluino1800',
+    'gluino2000',
+    #'gluino2200',
+    #'gluino2400',
+    #'gluino2600',
+]
+
+InputCondorArguments = {}
