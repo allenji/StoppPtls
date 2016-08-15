a536195b6c21a76441908f13196ccb50b5022f26
Juliette Alimena <juliette.alimena@cern.ch>
2016-08-15 09:42:41 -0400

    update datasets

--- Collection/test/localConfig_data.py
+++ Collection/test/localConfig_data.py
@@ -7,7 +7,8 @@ config_file = "delayedMuonsCandiate2016_cfg.py"
 #config_file = "delayedMuonsCandiate_cfg.py"
 
 datasets = [
-    "NoBPTX_2016BCDEF_PromptReco",
+    #"NoBPTX_2016BCDEF_PromptReco",
+    "NoBPTX_2016DE_PromptReco_Cosmic_TopAndBottom",
     #"NoBPTX_16Dec2015",
     #"NoBPTX_2015BCDE_PromptReco",
 ]
