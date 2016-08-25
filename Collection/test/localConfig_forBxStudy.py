d86d251d2ef1f4dc92d5d9ed9a73e8a2dc2dea3b
Juliette Alimena <juliette.alimena@cern.ch>
2016-08-25 04:51:52 -0400

    update datasets

--- Collection/test/localConfig_forBxStudy.py
+++ Collection/test/localConfig_forBxStudy.py
@@ -6,7 +6,8 @@ intLumi = 2830
 config_file = "stoppPtlsCandidate_Data_2016_forBxStudy_cfg.py"
 
 datasets = [
-    "NoBPTX_2016D_PromptReco",
+    "NoBPTX_2016B_PromptReco",
+    #"NoBPTX_2016D_PromptReco",
 ]
     
 InputCondorArguments = {}
