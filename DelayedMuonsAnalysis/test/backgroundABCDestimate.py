62762167d6a6632596624f7a3d6e54f216c572ff
allenji <weifengji@yahoo.com>
2016-09-01 12:51:45 -0400

    modify the name of channels

--- DelayedMuonsAnalysis/test/backgroundABCDestimate.py
+++ DelayedMuonsAnalysis/test/backgroundABCDestimate.py
@@ -38,24 +38,25 @@ else:
 
 for dataset in datasets:
     #outputFile = TFile(condor_dir+"/"+dataset+"_OnlyFilledRuns.root","RECREATE")
-    (numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionCutFlowPlotter")
-    #(numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionAUpperP50")
-    #(numB, errB) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionBUpperP50")
-    #(numC, errC) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionCUpperP50")
-    #(numD, errD) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionDUpperP50")
+    #(numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionCutFlowPlotter")
+    (numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionAUpperP50CutFlowPlotter")
+    (numB, errB) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionBUpperP50CutFlowPlotter")
+    (numC, errC) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionCUpperP50CutFlowPlotter")
+    (numD, errD) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionDUpperP50CutFlowPlotter")
 
     print "for " + dataset + ":"
     print "number of events in region A is: " + str(numA) + " +/- " + str(errA)
-    #print "number of events in region B is: " + numB + " +/- " + errB
-    #print "number of events in region C is: " + numC + " +/- " + errC
-    #print "number of events in region D is: " + numD + " +/- " + errD
+    print "number of events in region B is: " + str(numB) + " +/- " + str(errB)
+    print "number of events in region C is: " + str(numC) + " +/- " + str(errC)
+    print "number of events in region D is: " + str(numD) + " +/- " + str(errD)
 
-    #if numA!=0.:
-        #background_estimate = 1.0*numB*numC/numA
-    #else:
-        #print "numA is zero!!!"
-    #background_error = background_estimate*(sqrt( (errA/numA)*(errA/numA)+(errB/numB)*(errB/numB)+(errC/numC)*(errC/numC) ))
-    #print "background estimate (B*C/A) for " + dataset + " is: " + background_estimate + " +/- " + background_error
+    if numA!=0.:
+        background_estimate = 1.0*numB*numC/numA
+    else:
+        print "numA is zero!!!"
+    background_error = background_estimate*(sqrt( (errA/numA)*(errA/numA)+(errB/numB)*(errB/numB)+(errC/numC)*(errC/numC) ))
+    print "background estimate (B*C/A) for " + dataset + " is: " + str(background_estimate) + " +/- " + str(background_error)
 
     #plots = MakeOnlyFilledRunsHist(dataset,channel,arguments.hist)
     #outputFile.Close()
+print "done"
