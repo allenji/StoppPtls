6b1e929c78651cb091d8df51a194ac66087fdf05
allenji <weifengji@yahoo.com>
2016-09-10 10:36:15 -0400

    add more thresholds

--- DelayedMuonsAnalysis/test/backgroundABCDestimate.py
+++ DelayedMuonsAnalysis/test/backgroundABCDestimate.py
@@ -35,28 +35,40 @@ else:
     print "No condor output directory specified, shame on you"
     sys.exit(0)
 
+pt_threshold = [50, 60, 110, 150, 170, 200, 250, 300, 400]
 
 for dataset in datasets:
-    #outputFile = TFile(condor_dir+"/"+dataset+"_OnlyFilledRuns.root","RECREATE")
-    #(numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionCutFlowPlotter")
-    (numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionAUpperP50CutFlowPlotter")
-    (numB, errB) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionBUpperP50CutFlowPlotter")
-    (numC, errC) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionCUpperP50CutFlowPlotter")
-    (numD, errD) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionDUpperP50CutFlowPlotter")
+    for pt in pt_threshold:
+        #outputFile = TFile(condor_dir+"/"+dataset+"_OnlyFilledRuns.root","RECREATE")
+        #(numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionCutFlowPlotter")
+        regionA = "DelayedMuonsUpperLowerSelectionRegionAUpperP%dCutFlowPlotter"%(int(pt))
+        regionB = "DelayedMuonsUpperLowerSelectionRegionBUpperP%dCutFlowPlotter"%(int(pt))
+        regionC = "DelayedMuonsUpperLowerSelectionRegionCUpperP%dCutFlowPlotter"%(int(pt))
+        regionD = "DelayedMuonsUpperLowerSelectionRegionDUpperP%dCutFlowPlotter"%(int(pt))
+        #(numA, errA) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionAUpperP50CutFlowPlotter")
+        (numA, errA) = getYield(dataset,condor_dir, regionA)
+        #(numB, errB) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionBUpperP50CutFlowPlotter")
+        (numB, errB) = getYield(dataset,condor_dir, regionB)
+        #(numC, errC) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionCUpperP50CutFlowPlotter")
+        (numC, errC) = getYield(dataset,condor_dir, regionC)
+        #(numD, errD) = getYield(dataset,condor_dir,"DelayedMuonsUpperLowerSelectionRegionDUpperP50CutFlowPlotter")
+        (numD, errD) = getYield(dataset,condor_dir, regionD)
 
-    print "for " + dataset + ":"
-    print "number of events in region A is: " + str(numA) + " +/- " + str(errA)
-    print "number of events in region B is: " + str(numB) + " +/- " + str(errB)
-    print "number of events in region C is: " + str(numC) + " +/- " + str(errC)
-    print "number of events in region D is: " + str(numD) + " +/- " + str(errD)
+        print "///////////////////////////////////////////////////////////"
+        print "for " + dataset + " and pt threshold " + str(pt) + " GeV:"
+        print "number of events in region A is: " + str(numA) + " +/- " + str(errA)
+        print "number of events in region B is: " + str(numB) + " +/- " + str(errB)
+        print "number of events in region C is: " + str(numC) + " +/- " + str(errC)
+        print "number of events in region D is: " + str(numD) + " +/- " + str(errD)
 
-    if numA!=0.:
-        background_estimate = 1.0*numB*numC/numA
-    else:
-        print "numA is zero!!!"
-    background_error = background_estimate*(sqrt( (errA/numA)*(errA/numA)+(errB/numB)*(errB/numB)+(errC/numC)*(errC/numC) ))
-    print "background estimate (B*C/A) for " + dataset + " is: " + str(background_estimate) + " +/- " + str(background_error)
+        if numA!=0.:
+            background_estimate = 1.0*numB*numC/numA
+        else:
+            print "numA is zero!!!"
+        background_error = background_estimate*(sqrt( (errA/numA)*(errA/numA)+(errB/numB)*(errB/numB)+(errC/numC)*(errC/numC) ))
+        print "background estimate (B*C/A) for " + dataset + " is: " + str(background_estimate) + " +/- " + str(background_error)
+        print "///////////////////////////////////////////////////////////"
 
-    #plots = MakeOnlyFilledRunsHist(dataset,channel,arguments.hist)
-    #outputFile.Close()
+        #plots = MakeOnlyFilledRunsHist(dataset,channel,arguments.hist)
+        #outputFile.Close()
 print "done"
