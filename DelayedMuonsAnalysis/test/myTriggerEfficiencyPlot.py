0f173f4d5ea0fbd0df25a354cf20b700de318092
Juliette Alimena <juliette.alimena@cern.ch>
2016-10-20 09:17:22 -0400

    update datasets

--- DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot.py
+++ DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot.py
@@ -3,21 +3,22 @@ intLumi = 2460
 cutName = 'trigger'
 input_sources = [
     { 'condor_dir' : 'NoBPTX2016Data_TriggerTurnOn',
-      'dataset' :   'NoBPTX_2016BCD_PromptReco_OnlyControlTriggers_Ntuple',
+      'dataset' :   'NoBPTX_2016BCDEFG_PromptReco_OnlyControlTriggers_Ntuple',
       'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
       'num_channel' : 'PreSelectionUpperLowerTurnOnNum35',
-      'legend_entry' : 'Run2016B-D, L2Mu35',
+      'legend_entry' : 'Run2016B-G, L2Mu35',
       'color' : 'black',
       #'fill' : '',
       'marker' : 'circle',
       },
     { 'condor_dir' : 'NoBPTX2016Data_TriggerTurnOn',
-      'dataset' :   'NoBPTX_2016BCD_PromptReco_OnlyControlTriggers_Ntuple',
+      'dataset' :   'NoBPTX_2016BCDEFGH_PromptReco_OnlyControlTriggers_Ntuple',
       'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
       'num_channel' : 'PreSelectionUpperLowerTurnOnNum40',
-      'legend_entry' : 'Run2016B-D, L2Mu40',
+      'legend_entry' : 'Run2016B-H, L2Mu40',
       'color' : 'red',
       #'fill' : '',
       'marker' : 'square',
       },
+
 ]
