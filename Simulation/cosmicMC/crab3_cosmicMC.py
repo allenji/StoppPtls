f947922a4bd30327648cf07a0ad999f9dbe9d2db
Juliette Alimena <juliette.alimena@cern.ch>
2016-11-21 08:25:44 -0500

    update cosmic MC to use 0 to 25 ns timing

--- Simulation/cosmicMC/crab3_cosmicMC.py
+++ Simulation/cosmicMC/crab3_cosmicMC.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'Cosmic_fullDetector_adjustedTiming_MCRUN2_7122_V5'
+config.General.requestName = 'Cosmic_fullDetector_0To25Timing_MCRUN2_7122_V5'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -12,10 +12,10 @@ config.JobType.psetName = 'step1_cosmicMC_cfg.py'
 config.Data.outputPrimaryDataset = 'Cosmic'
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = 500
-NJOBS = 2000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+NJOBS = 5000  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
 config.Data.publication = True
-config.Data.outputDatasetTag = 'Cosmic_fullDetector_adjustedTiming_MCRUN2_7122_V5'
+config.Data.outputDatasetTag = 'Cosmic_fullDetector_0To25Timing_MCRUN2_7122_V5'
 
 config.Site.storageSite = 'T3_US_OSU'
