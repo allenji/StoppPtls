74363fa4f542cc1b0df3ee20c632817e8261c25a
allenji <weifengji@yahoo.com>
2017-01-17 07:01:51 -0500

    DelayedMuon ToyMC update

--- ToyMC_DelayedMuons/python/Histograms.py
+++ ToyMC_DelayedMuons/python/Histograms.py
@@ -1,4 +1,17 @@
 import FWCore.ParameterSet.Config as cms
+ToyMCRunLbHistogram = cms.PSet(
+    inputCollection = cms.vstring("eventvariables"),
+    histograms = cms.VPSet(
+        cms.PSet (
+            name = cms.string("runlb"),
+            title = cms.string("Run vs LS; Run number; lumi section"),
+            binsX = cms.untracked.vdouble(7000,254000, 261000),
+            #binsX = cms.untracked.vdouble(45000, 235000, 280000),
+            binsY = cms.untracked.vdouble(4000, 0, 4000),
+            inputVariables = cms.vstring("run","lb"),
+            )
+    )
+)
 ToyMCRunLbHistogram_2016 = cms.PSet(
     inputCollection = cms.vstring("eventvariables"),
     histograms = cms.VPSet(
