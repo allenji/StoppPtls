763bf0a0b26ed218f2fe942041e9d7aa5ee371aa
Juliette Alimena <juliette.alimena@cern.ch>
2017-02-01 09:26:33 -0500

    update trigger turn ons to run over 2016 rereco

--- DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot_systL2Mu35.py
+++ DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot_systL2Mu35.py
@@ -2,23 +2,26 @@
 intLumi = 2460
 cutName = 'trigger'
 input_sources = [
-    { 'condor_dir' : 'TriggerTurnOn_2016Systematic',
+    { #'condor_dir' : 'TriggerTurnOn_2016Systematic',
+        'condor_dir' : 'TriggerTurnOn_2016Systematic_2016ReReco',
+      #'dataset' :   'NoBPTX_2016BCDEFG_PromptReco_OnlyControlTriggers_Ntuple',
+      'dataset' :   'NoBPTX_2016BCDEFG_23Sep2016_OnlyControlTriggers_Ntuple',
+      'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
+      'num_channel' : 'PreSelectionUpperLowerTurnOnNum35',
+      'legend_entry' : 'Run2016B-G, L2Mu35',
+      'color' : 'black',
+      #'fill' : '',
+      'marker' : 'circle',
+      },
+
+    { #'condor_dir' : 'TriggerTurnOn_2016Systematic',
+        'condor_dir' : 'TriggerTurnOn_2016Systematic_2016ReReco',
       'dataset' :   'cosmicMC_0To25Timing_onlyControlTriggers_Ntuple_2016',
       'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
       'num_channel' : 'PreSelectionUpperLowerTurnOnNum35',
       'legend_entry' : 'Cosmic Simulation, 2016, L2Mu35',
       'color' : 'blue',
       'fill' : 'hollow',
-      'marker' : 'circle',
-      },
-
-    { 'condor_dir' : 'TriggerTurnOn_2016Systematic',
-      'dataset' :   'NoBPTX_2016BCDEFG_PromptReco_OnlyControlTriggers_Ntuple',
-      'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
-      'num_channel' : 'PreSelectionUpperLowerTurnOnNum35',
-      'legend_entry' : 'Run2016B-G, L2Mu35',
-      'color' : 'black',
-      #'fill' : '',
       'marker' : 'circle',
       },
 
--- DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot_systL2Mu40.py
+++ DelayedMuonsAnalysis/test/myTriggerEfficiencyPlot_systL2Mu40.py
@@ -2,8 +2,10 @@
 intLumi = 2460
 cutName = 'trigger'
 input_sources = [
-    { 'condor_dir' : 'TriggerTurnOn_2016Systematic',
-      'dataset' :   'NoBPTX_2016BCDEFGH_PromptReco_OnlyControlTriggers_Ntuple',
+    { #'condor_dir' : 'TriggerTurnOn_2016Systematic',
+        'condor_dir' : 'TriggerTurnOn_2016Systematic_2016ReReco',
+      #'dataset' :   'NoBPTX_2016BCDEFGH_PromptReco_OnlyControlTriggers_Ntuple',
+      'dataset' :   'NoBPTX_2016BCDEFGH_23Sep2016_OnlyControlTriggers_Ntuple',
       'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
       'num_channel' : 'PreSelectionUpperLowerTurnOnNum40',
       'legend_entry' : 'Run2016B-H, L2Mu40',
@@ -12,7 +14,8 @@ input_sources = [
       'marker' : 'square',
       },
 
-    { 'condor_dir' : 'TriggerTurnOn_2016Systematic',
+    { #'condor_dir' : 'TriggerTurnOn_2016Systematic',
+        'condor_dir' : 'TriggerTurnOn_2016Systematic_2016ReReco',
       'dataset' :   'cosmicMC_0To25Timing_onlyControlTriggers_Ntuple_2016',
       'den_channel' : 'PreSelectionUpperLowerTurnOnDen',
       'num_channel' : 'PreSelectionUpperLowerTurnOnNum40',
