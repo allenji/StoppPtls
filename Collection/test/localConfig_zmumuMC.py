82f6f2ac5744c6582577c28740cea9525e796de6
Juliette Alimena <juliette.alimena@cern.ch>
2017-04-29 14:03:04 -0400

    fix

--- Collection/test/localConfig_zmumuMC.py
+++ Collection/test/localConfig_zmumuMC.py
@@ -4,10 +4,10 @@ from OSUT3Analysis.Configuration.RECOSamples import *
 intLumi = 2460
 
 
-config_file = "delayedMuonsCandiate_zmumuMC_2016_cfg.py"
+config_file = "delayedMuonsCandiate_zmumuMC_80To100_2016_cfg.py"
 
 datasets = [
-    'ZToMuMu_50To120_80X'
+    'ZToMuMu_50To120_80X_AODSIM'
 ]
     
 InputCondorArguments = {}
