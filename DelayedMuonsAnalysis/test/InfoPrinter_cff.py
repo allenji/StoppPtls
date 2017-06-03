a2a4c0831fbc96ddfbb17a32431310b1a9b2ac37
Juliette Alimena <juliette.alimena@cern.ch>
2017-06-03 10:44:01 -0400

    useful Delayed Muons printouts

---
+++ DelayedMuonsAnalysis/test/InfoPrinter_cff.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+
+infoPrinter = cms.EDAnalyzer ("InfoPrinter",
+################################################################################
+# N.B.: one of the following three options must be set in order to print out
+# extra information
+    eventsToPrint               =  cms.VEventID (
+#        cms.EventID (1, 16, 2686),
+    ),
+    printAllEvents              =  cms.bool  (False),  # print information for all events, overriding eventsToPrint
+    printPassedEvents           =  cms.bool  (True),  # print information for events that pass all cuts
+################################################################################
+
+    valuesToPrint               =  cms.VPSet (
+#        cms.PSet (
+#            inputCollection = cms.vstring ("jets"),
+#            valueToPrint = cms.string ("pt")
+#        ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("p")
+            ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("pt")
+            ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("eta")
+            ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("phi")
+            ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("dtTofTimeAtIpInOut")
+            ),
+        cms.PSet (
+            inputCollection = cms.vstring ("tracks"),
+            valueToPrint = cms.string ("rpcHitBxAverage")
+            ),
+    ),
+    printIndividualObjectFlags  =  cms.bool  (False),  # print the noncumulative flags for each object
+    printCumulativeObjectFlags  =  cms.bool  (False),  # print the cumulative flags for each object
+    printTriggerFlags           =  cms.bool  (False),  # print the decision for each trigger
+    printVetoTriggerFlags       =  cms.bool  (False),  # print the decision for each trigger to veto
+    printTriggerFilterFlags     =  cms.bool  (False),  # print the decision for each trigger filter
+    printTriggerInMenuFlags     =  cms.bool  (False),  # print the decision for each trigger required to be in the menu
+    printMETFilterFlags         =  cms.bool  (False),  # print the decision for each MET filter
+    printIndividualEventFlags   =  cms.bool  (False),  # print the decision for each cut, independent of any other cuts
+    printCumulativeEventFlags   =  cms.bool  (False),  # print the decision for each cut, based on objects selected by previous cuts
+    printTriggerDecision        =  cms.bool  (False),  # print whether the event passes the triggers
+    printMETFilterDecision      =  cms.bool  (False),  # print whether the event passes the MET filters
+    printCutDecision            =  cms.bool  (False),  # print whether the event passes all cuts, not including the triggers or MET filters
+    printEventDecision          =  cms.bool  (False),  # print whether the event passes the triggers, MET filters, and all cuts
+    printAllTriggers            =  cms.bool  (False),  # print all triggers in the event
+    printAllTriggerFilters      =  cms.bool  (False),  # print all trigger filters in the event
+    printAllMETFilters          =  cms.bool  (False),  # print all MET filters in the event
+)
